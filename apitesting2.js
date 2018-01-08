bb988b336ae778d01a8719abcd46a2bc1df94b92
Hyrla Miranda <hyrlammiranda@gmail.com>
2018-01-08 11:01:42 -0200

    Creating an simple API testing, my first api testing

---
+++ apitesting2.js
@@ -0,0 +1,19 @@
+
+var frase;
+
+carregaFrase = function(callback) {
+    setTimeout(function(){
+        //Simula leitura da frase no banco de dados
+        frase = 'Minha frase obstrutiva';
+        callback();
+
+    }, 3000)
+}
+
+imprimeFrase = function() {
+    console.log(frase);
+}
+
+carregaFrase(imprimeFrase);
+
+console.log("I am wonder woman");
